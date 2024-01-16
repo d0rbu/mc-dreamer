313c828e947a1c4e52690ab85e5a6b7b8a62b558
d0rb <hacperu2010@gmail.com>
2024-01-16 13:53:43 -0600

    process literal edge and corner cases

--- core/extract/extract.py
+++ core/extract/extract.py
@@ -4,12 +4,14 @@ import asyncio
 import anvil
 import numpy as np
 from functools import partial
-from itertools import chain
+from itertools import chain, product
 from typing import Sequence
 
 
-CHUNK_SIZE = (16, 16, 256)  # in blocks
-REGION_SIZE = (32, 32)  # in chunks
+CHUNK_LENGTH = 16  # in blocks
+CHUNK_HEIGHT = 256  # in blocks
+REGION_LENGTH = 32  # in chunks
+REGION_BLOCK_LENGTH = REGION_LENGTH * CHUNK_LENGTH  # in blocks
 
 def get_available_regions(region_dir: str | os.PathLike) -> tuple[np.ndarray, Sequence[tuple[int, int]]]:
     region_files = [file for file in os.listdir(region_dir) if file.endswith(".mca")]
@@ -53,18 +55,94 @@ def extract_from_region(
 ) -> None:
     with open(file_path, "rb") as region_file:
         region = anvil.Region.from_file(region_file)
-    
-    # ndarray_blocks = np.empty((REGION_SIZE[0] * CHUNK_SIZE[0], REGION_SIZE[1] * CHUNK_SIZE[1], CHUNK_SIZE[2]), dtype=np.uint16)
-    ndarray_blocks = np.empty((REGION_SIZE[0] * CHUNK_SIZE[0], REGION_SIZE[1] * CHUNK_SIZE[1], CHUNK_SIZE[2]), dtype=object)
-    for x in range(REGION_SIZE[0]):
-        for z in range(REGION_SIZE[1]):
-            chunk = anvil.Chunk.from_region(region, (x, z))
-            for block_x in range(x, x + CHUNK_SIZE[0]):
-                for block_z in range(z, z + CHUNK_SIZE[1]):
-                    for block_y in range(CHUNK_SIZE[2]):
-                        ndarray_blocks[block_x, block_z, block_y] = chunk.get_block(block_x, block_y, block_z).name()
+
+    # ndarray_blocks = np.empty((REGION_BLOCK_LENGTH, CHUNK_HEIGHT, REGION_BLOCK_LENGTH), dtype=np.uint16)
+    ndarray_blocks = np.empty((REGION_BLOCK_LENGTH, CHUNK_HEIGHT, REGION_BLOCK_LENGTH), dtype=object)
+    for x, z in product(range(REGION_LENGTH), repeat=2):
+        chunk = anvil.Chunk.from_region(region, (x, z))
+        for block_x, block_y, block_z in product(range(x, x + CHUNK_LENGTH), range(CHUNK_HEIGHT), range(z, z + CHUNK_LENGTH)):
+            ndarray_blocks[block_x, block_y, block_z] = chunk.get_block(block_x, block_y, block_z).name()
     
     future.set_result(extract_from_ndarray(ndarray_blocks))
+
+def get_edge_samples(
+    file_paths: tuple[str | os.PathLike, str | os.PathLike],
+    axis: int = 0,  # which axis to stitch the samples along, should be 0 or 2
+) -> np.ndarray:
+    regions = []
+    for file_path in file_paths:
+        with open(file_path, "rb") as region_file:
+            regions.append(anvil.Region.from_file(region_file))
+
+    region_extracted_size = [REGION_BLOCK_LENGTH, CHUNK_HEIGHT, REGION_BLOCK_LENGTH]
+    region_extracted_size[axis] = CHUNK_LENGTH
+    
+    # first_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    # second_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    first_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+    second_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+
+    for block_x, block_y, block_z in product(*[range(-size, 0) for size in region_extracted_size]):
+        block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
+        first_ndarray_region[block_x, block_y, block_z] = regions[0].get_block(block_x, block_y, block_z).name()
+    
+    for block_x, block_y, block_z in product(*[range(size) for size in region_extracted_size]):
+        block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
+        second_ndarray_region[block_x, block_y, block_z] = regions[1].get_block(block_x, block_y, block_z).name()
+    
+    extracted_edge_region = np.concatenate((first_ndarray_region, second_ndarray_region), axis=axis)
+    sliced_edge_slices = [
+        slice(0, axis_size)
+        for axis_size in extracted_edge_region.shape
+    ]
+    sliced_edge_slices[axis] = slice(1, -1)
+    sliced_edge_region = extracted_edge_region[sliced_edge_slices]  # cut off the edges of the region that were already previously extracted
+
+    return sliced_edge_region
+
+def get_corner_samples(
+    file_paths: tuple[str | os.PathLike, str | os.PathLike, str | os.PathLike, str | os.PathLike],
+) -> np.ndarray:  # filepaths should be in the order of top left, top right, bottom left, bottom right
+    regions = []
+    for file_path in file_paths:
+        with open(file_path, "rb") as region_file:
+            regions.append(anvil.Region.from_file(region_file))
+    
+    region_extracted_size = [CHUNK_LENGTH, CHUNK_HEIGHT, CHUNK_LENGTH]
+
+    # first_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    # second_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    # third_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    # fourth_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    first_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+    second_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+    third_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+    fourth_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+
+    for block_x, block_y, block_z in product(*[range(-size, 0) for size in region_extracted_size]):
+        block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
+        first_ndarray_region[block_x, block_y, block_z] = regions[0].get_block(block_x, block_y, block_z).name()
+    
+    for block_x, block_y, block_z in product(range(region_extracted_size[0]), range(CHUNK_HEIGHT), range(-region_extracted_size[2], 0)):
+        block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
+        second_ndarray_region[block_x, block_y, block_z] = regions[1].get_block(block_x, block_y, block_z).name()
+    
+    for block_x, block_y, block_z in product(range(-region_extracted_size[0], 0), range(CHUNK_HEIGHT), range(region_extracted_size[2])):
+        block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
+        third_ndarray_region[block_x, block_y, block_z] = regions[2].get_block(block_x, block_y, block_z).name()
+    
+    for block_x, block_y, block_z in product(range(region_extracted_size[0]), range(CHUNK_HEIGHT), range(region_extracted_size[2])):
+        block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
+        fourth_ndarray_region[block_x, block_y, block_z] = regions[3].get_block(block_x, block_y, block_z).name()
+    
+    extracted_corner_region = np.concatenate((
+        np.concatenate((first_ndarray_region, second_ndarray_region), axis=0),
+        np.concatenate((third_ndarray_region, fourth_ndarray_region), axis=0),
+    ), axis=2)
+
+    sliced_corner_region = extracted_corner_region[1:-1, :, 1:-1]  # cut off the edges of the region that were already previously extracted
+
+    return sliced_corner_region
 
 async def extract_world_async(
     path: str | os.PathLike,
@@ -89,9 +167,34 @@ async def extract_world_async(
     get_all_samples_future = asyncio.gather(*extracted_sample_futures)
     get_all_samples_future.add_done_callback(process_samples_callback)
 
-    samples = await get_all_samples_future
+    await get_all_samples_future
     
-    vertical_edge_samples
+    vertical_edge_region_samples = available_regions[:-1] & available_regions[1:]
+    horizontal_edge_region_samples = available_regions[:, :-1] & available_regions[:, 1:]
+    corner_region_samples = available_regions[:-1, :-1] & available_regions[:-1, 1:] & available_regions[1:, :-1] & available_regions[1:, 1:]
+
+    unprocessed_samples = []
+    for x, z in chain(np.nonzero(vertical_edge_region_samples), np.nonzero(horizontal_edge_region_samples)):
+        file_paths = (
+            os.path.join(region_dir, f"r.{x}.{z}.mca"),
+            os.path.join(region_dir, f"r.{x + 1}.{z}.mca"),
+        )
+        unprocessed_samples.append(get_edge_samples(file_paths, axis=0))
+    
+    for x, z in np.nonzero(corner_region_samples):
+        file_paths = (
+            os.path.join(region_dir, f"r.{x}.{z}.mca"),
+            os.path.join(region_dir, f"r.{x + 1}.{z}.mca"),
+            os.path.join(region_dir, f"r.{x}.{z + 1}.mca"),
+            os.path.join(region_dir, f"r.{x + 1}.{z + 1}.mca"),
+        )
+        unprocessed_samples.append(get_corner_samples(file_paths))
+    
+    extracted_sample_futures = []
+    for unprocessed_sample in unprocessed_samples:
+        future = asyncio.Future()
+        extracted_sample_futures.append(future)
+        # TODO: process the samples
 
 def extract_world(
     path: str | os.PathLike,
