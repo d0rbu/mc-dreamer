689f2e0cefd06b7c389dd3c789b9fbe05b892221
d0rb <hacperu2010@gmail.com>
2024-02-13 10:54:20 -0600

    set up skeleton for external sort

--- scripts/visualize_train_data.py
+++ scripts/visualize_train_data.py
@@ -2,19 +2,15 @@ import os
 import pickle
 import numpy as np
 import torch as th
+from tempfile import TemporaryDirectory
 from nbtschematic import SchematicFile
 from tqdm import tqdm
 
-def convert_percentiles(
+def get_percentile_scores(
+    extraction_output_dir: str | os.PathLike,
     percentiles: th.Tensor,
-    name: str | None = None,
-    extraction_output_dir: str | os.PathLike = "outputs",
-    schematic_dir: str | os.PathLike = "schematics_output",
-) -> None:
-    full_scores: dict[tuple[str, int], float] = {}  # maps (file, index) to score
-
-    if name is not None:
-        extraction_output_dir = os.path.join(extraction_output_dir, name)
+) -> list[tuple[str, int, float]]:
+    full_scores: list[tuple[str, int, float]] = []  # (file, index, score)
 
     for root, subdirs, files in tqdm(os.walk(extraction_output_dir), desc="Gathering scores"):
         if not files:
@@ -29,19 +25,44 @@ def convert_percentiles(
                 scores = pickle.load(f)
             
             for i, score in enumerate(scores):
-                full_scores[(file_path, i)] = score
-    
-    sorted_scores = sorted(full_scores.items(), key=lambda x: x[1])
-    num_samples = len(sorted_scores)
+                full_scores.append((file_path, i, score))
+
+    full_scores = sorted(full_scores.items(), key=lambda x: x[1])
+    num_samples = len(full_scores)
     percentiles = percentiles * num_samples
     percentiles[-1] = num_samples - 1
     percentiles = percentiles.to(int)
     percentiles = percentiles.unique()
 
+    selected_scores = [full_scores[i] for i in percentiles]
+    
+    return selected_scores
+
+def get_percentile_scores_external(
+    extraction_output_dir: str | os.PathLike,
+    percentiles: th.Tensor,
+    num_external_sort_files: int,
+) -> list[tuple[str, int, float]]:
+    raise NotImplementedError("External sort not implemented yet")
+
+def convert_percentiles(
+    percentiles: th.Tensor,
+    name: str | None = None,
+    extraction_output_dir: str | os.PathLike = "outputs",
+    schematic_dir: str | os.PathLike = "schematics_output",
+    num_external_sort_files: int = 0,  # max number of files at a time for external sort
+) -> None:
+    if name is not None:
+        extraction_output_dir = os.path.join(extraction_output_dir, name)
+
+    if num_external_sort_files > 0:
+        selected_scores = get_percentile_scores_external(extraction_output_dir, percentiles, num_external_sort_files)
+    else:
+        selected_scores = get_percentile_scores(extraction_output_dir, percentiles)
+
     loaded_samples = {}
 
-    for percentile in tqdm(percentiles, desc="Gathering schematics"):
-        (scores_path, i), score = sorted_scores[percentile]
+    for (scores_path, i), score in tqdm(selected_scores, desc="Gathering schematics"):
         file_path = scores_path.replace(".scores", ".pt")
 
         if file_path in loaded_samples:
@@ -55,7 +76,7 @@ def convert_percentiles(
         sf.blocks = sample.numpy().astype(np.uint8).transpose(1, 2, 0)
 
         sf.save(os.path.join(schematic_dir, f"{score:.5f}.schematic"))
-    
+
     print("Done!")
 
 if __name__ == "__main__":
