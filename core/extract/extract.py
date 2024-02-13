e7987729ce89e38a8987e7930a227ff259ac1604
d0rbu <henryandrecastillo@gmail.com>
2024-02-12 23:04:34 -0600

    fix edge and corner cases properly extract from ndarray instead of region

--- core/extract/extract.py
+++ core/extract/extract.py
@@ -149,80 +149,64 @@ def get_edge_samples(
     file_paths: tuple[str | os.PathLike, str | os.PathLike],
     axis: int = 0,  # which axis to stitch the samples along, should be 0 or 2
 ) -> np.ndarray:
-    regions = []
+    region_ndarrays = []
     for file_path in file_paths:
-        with open(file_path, "rb") as region_file:
-            regions.append(anvil.Region.from_file(region_file))
+        region_ndarrays.append(np.load(file_path))
 
     region_extracted_size = [REGION_BLOCK_LENGTH, CHUNK_HEIGHT, REGION_BLOCK_LENGTH]
-    region_extracted_size[axis] = CHUNK_LENGTH
+    region_extracted_size[axis] = CHUNK_LENGTH - 1
     
-    # first_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
-    # second_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
-    first_ndarray_region = np.empty(*region_extracted_size, dtype=object)
-    second_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+    first_ndarray_region = np.empty(*region_extracted_size, dtype=np.ubyte)
+    second_ndarray_region = np.empty(*region_extracted_size, dtype=np.ubyte)
 
     for block_x, block_y, block_z in product(*[range(-size, 0) for size in region_extracted_size]):
         block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
-        first_ndarray_region[block_x, block_y, block_z] = regions[0].get_block(block_x, block_y, block_z).name()
+        first_ndarray_region[block_x, block_y, block_z] = region_ndarrays[0][block_x, block_y, block_z]
     
     for block_x, block_y, block_z in product(*[range(size) for size in region_extracted_size]):
         block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
-        second_ndarray_region[block_x, block_y, block_z] = regions[1].get_block(block_x, block_y, block_z).name()
+        second_ndarray_region[block_x, block_y, block_z] = region_ndarrays[1][block_x, block_y, block_z]
     
     extracted_edge_region = np.concatenate((first_ndarray_region, second_ndarray_region), axis=axis)
-    sliced_edge_slices = [
-        slice(0, axis_size)
-        for axis_size in extracted_edge_region.shape
-    ]
-    sliced_edge_slices[axis] = slice(1, -1)
-    sliced_edge_region = extracted_edge_region[sliced_edge_slices]  # cut off the edges of the region that were already previously extracted
 
-    return sliced_edge_region
+    return extracted_edge_region
 
 def get_corner_samples(
     file_paths: tuple[str | os.PathLike, str | os.PathLike, str | os.PathLike, str | os.PathLike],
 ) -> np.ndarray:  # filepaths should be in the order of top left, top right, bottom left, bottom right
-    regions = []
+    region_ndarrays = []
     for file_path in file_paths:
-        with open(file_path, "rb") as region_file:
-            regions.append(anvil.Region.from_file(region_file))
+        region_ndarrays.append(np.load(file_path))
     
-    region_extracted_size = [CHUNK_LENGTH, CHUNK_HEIGHT, CHUNK_LENGTH]
+    region_extracted_size = [CHUNK_LENGTH - 1, CHUNK_HEIGHT, CHUNK_LENGTH - 1]
 
-    # first_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
-    # second_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
-    # third_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
-    # fourth_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
-    first_ndarray_region = np.empty(*region_extracted_size, dtype=object)
-    second_ndarray_region = np.empty(*region_extracted_size, dtype=object)
-    third_ndarray_region = np.empty(*region_extracted_size, dtype=object)
-    fourth_ndarray_region = np.empty(*region_extracted_size, dtype=object)
+    first_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    second_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    third_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
+    fourth_ndarray_region = np.empty(*region_extracted_size, dtype=np.uint16)
 
     for block_x, block_y, block_z in product(*[range(-size, 0) for size in region_extracted_size]):
         block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
-        first_ndarray_region[block_x, block_y, block_z] = regions[0].get_block(block_x, block_y, block_z).name()
+        first_ndarray_region[block_x, block_y, block_z] = region_ndarrays[0][block_x, block_y, block_z]
     
     for block_x, block_y, block_z in product(range(region_extracted_size[0]), range(CHUNK_HEIGHT), range(-region_extracted_size[2], 0)):
         block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
-        second_ndarray_region[block_x, block_y, block_z] = regions[1].get_block(block_x, block_y, block_z).name()
+        second_ndarray_region[block_x, block_y, block_z] = region_ndarrays[1][block_x, block_y, block_z]
     
     for block_x, block_y, block_z in product(range(-region_extracted_size[0], 0), range(CHUNK_HEIGHT), range(region_extracted_size[2])):
         block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
-        third_ndarray_region[block_x, block_y, block_z] = regions[2].get_block(block_x, block_y, block_z).name()
+        third_ndarray_region[block_x, block_y, block_z] = region_ndarrays[2][block_x, block_y, block_z]
     
     for block_x, block_y, block_z in product(range(region_extracted_size[0]), range(CHUNK_HEIGHT), range(region_extracted_size[2])):
         block_x, block_y, block_z = block_x % REGION_BLOCK_LENGTH, block_y % CHUNK_HEIGHT, block_z % REGION_BLOCK_LENGTH
-        fourth_ndarray_region[block_x, block_y, block_z] = regions[3].get_block(block_x, block_y, block_z).name()
+        fourth_ndarray_region[block_x, block_y, block_z] = region_ndarrays[3][block_x, block_y, block_z]
     
     extracted_corner_region = np.concatenate((
         np.concatenate((first_ndarray_region, second_ndarray_region), axis=0),
         np.concatenate((third_ndarray_region, fourth_ndarray_region), axis=0),
     ), axis=2)
 
-    sliced_corner_region = extracted_corner_region[1:-1, :, 1:-1]  # cut off the edges of the region that were already previously extracted
-
-    return sliced_corner_region
+    return extracted_corner_region
 
 def save_samples(
     samples: list[np.ndarray],
@@ -297,10 +281,11 @@ def extract_world(
         if len(sample_index_chunk) <= rank:  # no more work left for us this iteration!
             break
 
-        x, z = sample_index_chunk[rank]
+        x = sample_index_chunk[rank][0]
+        z = sample_index_chunk[rank][1]
         file_paths = (
-            os.path.join(region_dir, f"r.{x}.{z}.mca"),
-            os.path.join(region_dir, f"r.{x + 1}.{z}.mca"),
+            os.path.join(region_dir, f"r.{x}.{z}.npy"),
+            os.path.join(region_dir, f"r.{x + 1}.{z}.npy"),
         )
         new_file_path = os.path.join(region_dir, f"r.{x}-{x + 1}.{z}.npy")
         if os.path.exists(new_file_path):
@@ -312,10 +297,11 @@ def extract_world(
         if len(sample_index_chunk) <= rank:  # no more work left for us this iteration!
             break
 
-        x, z = sample_index_chunk[rank]
+        x = sample_index_chunk[rank][0]
+        z = sample_index_chunk[rank][1]
         file_paths = (
-            os.path.join(region_dir, f"r.{x}.{z}.mca"),
-            os.path.join(region_dir, f"r.{x}.{z + 1}.mca"),
+            os.path.join(region_dir, f"r.{x}.{z}.npy"),
+            os.path.join(region_dir, f"r.{x}.{z + 1}.npy"),
         )
         new_file_path = os.path.join(region_dir, f"r.{x}.{z}-{z + 1}.npy")
         if os.path.exists(new_file_path):
@@ -327,12 +313,13 @@ def extract_world(
         if len(sample_index_chunk) <= rank:  # no more work left for us this iteration!
             break
 
-        x, z = sample_index_chunk[rank]
+        x = sample_index_chunk[rank][0]
+        z = sample_index_chunk[rank][1]
         file_paths = (
-            os.path.join(region_dir, f"r.{x}.{z}.mca"),
-            os.path.join(region_dir, f"r.{x + 1}.{z}.mca"),
-            os.path.join(region_dir, f"r.{x}.{z + 1}.mca"),
-            os.path.join(region_dir, f"r.{x + 1}.{z + 1}.mca"),
+            os.path.join(region_dir, f"r.{x}.{z}.npy"),
+            os.path.join(region_dir, f"r.{x + 1}.{z}.npy"),
+            os.path.join(region_dir, f"r.{x}.{z + 1}.npy"),
+            os.path.join(region_dir, f"r.{x + 1}.{z + 1}.npy"),
         )
         new_file_path = os.path.join(region_dir, f"r.{x}-{x + 1}.{z}-{z + 1}.npy")
         if os.path.exists(new_file_path):
