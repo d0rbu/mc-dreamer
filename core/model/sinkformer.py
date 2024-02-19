10b95beb8e742e6d967e8677c2f91374e223fbed
d0rb <hacperu2010@gmail.com>
2024-02-19 15:50:16 -0600

    start on skeleton of impl

--- core/model/sinkformer.py
+++ core/model/sinkformer.py
@@ -1,4 +1,21 @@
 import torch as th
+from typing import Self
 
 
-# TODO: implement transformer with sink k/v tokens at each attention layer and "absolute" position embeddings
+# TODO: subclass LlamaFlashAttention2 to use sink tokens and use passed absolute position
+
+
+class SinkFormer(th.nn.Module):
+    def __init__(self: Self) -> None:
+        super().__init__()
+
+    def forward(self: Self, x: th.Tensor, start_pos_indices: th.Tensor | None = None) -> th.Tensor:
+        """
+        Args:
+            x: (B, T, d_model)
+            start_pos_indices: (B, T) or None
+        Returns:
+            x: (B, T, d_model)
+        """
+        if start_pos_indices is None:
+            start_pos_indices = th.zeros(x.shape[:2], dtype=th.int32, device=x.device)
