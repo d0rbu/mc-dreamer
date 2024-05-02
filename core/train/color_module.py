ff27424687478bc34861cdfc7fcac2cf567d93cf
d0rb <hacperu2010@gmail.com>
2024-05-02 02:03:46 -0500

    more generator shenanigans

--- core/train/color_module.py
+++ core/train/color_module.py
@@ -327,7 +327,7 @@ class ColorModule(BitDiffusion):
         if inpaint:
             inpaint_schedule = [context]
 
-            for last_sigma, current_sigma in reversed(list(zip(schedule[:-1], schedule[1:]))):
+            for current_sigma, last_sigma in reversed(list(zip(schedule[:-1], schedule[1:]))):
                 noise = th.randn(shape, device = schedule.device)
                 sigma_delta = (current_sigma ** 2 - last_sigma ** 2) ** 0.5
 
